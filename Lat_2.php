0c9795a30c94461b8f79c41189926dc8a124b7cc
Evi Dianasari <evidnsr@gmail.com>
2023-04-25 23:20:16 +0800

    Update Lat_2.php

--- Lat_2.php
+++ Lat_2.php
@@ -1,7 +1,22 @@
+<!-- Segitiga asli -->
+<!-- <?php
+for($i=0;$i<=9;$i++){
+    for($j=0;$j<$i;$j++){
+        echo "*";
+    }
+    echo "<br/>";
+}
+?> -->
+
+
+<!-- Segitiga terbalik -->
 <?php
-
-
-
-
-
+for($i=9;$i>=0;$i--){
+    for($j=0;$j<$i;$j++){
+        echo "*";
+    }
+    echo "<br/>";
+}
 ?>
+
+<!-- Beserta buatlah komentar tiap line dari perintah yang ada, jelaskan maksud dari setiap baris scriptnya. -->
