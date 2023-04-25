15813522a18f16a44155b1d709f340d302c4a72c
Evi Dianasari <evidnsr@gmail.com>
2023-04-25 23:20:14 +0800

    Update Lat_1.php

--- Lat_1.php
+++ Lat_1.php
@@ -1,7 +1,19 @@
+<!-- hanya menggunakan konsep perulangan -->
 <?php
-
-
-
-
-
+for ($i = 1; $i <= 10; $i++) {
+    echo "1 x " . $i . " = " . (1 * $i) . "<br>";
+}
 ?>
+
+
+<!-- menggunakan konsep table -->
+<!-- <?php
+echo "<table border='0'>";
+for ($i = 1; $i <= 10; $i++) {
+    echo "<tr>";
+    echo "<td>1 x " . $i . "=</td>";
+    echo "<td>" . (1 * $i) . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?> -->
