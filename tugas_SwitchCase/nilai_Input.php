747a4af636a446fbfe57e55ad3f260502bdf96a7
Evi Dianasari <evidnsr@gmail.com>
2023-04-25 23:20:11 +0800

    Create nilai_Input.php

---
+++ tugas_SwitchCase/nilai_Input.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kategori Nilai</title>
+</head>
+<body>
+    <form method="post">
+        <label>Inputkan nilai Anda:</label>
+        <input type="number" name="nilai" required>
+        <button type="submit">Hitung</button>
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // ambil nilai dari form
+        $nilai = $_POST["nilai"];
+
+        // hitung kategori
+        switch(true) {
+            case ($nilai >= 90 && $nilai <= 100):
+                $kategori = "A";
+                break;
+            case ($nilai >= 80 && $nilai < 90):
+                $kategori = "B";
+                break;
+            case ($nilai >= 70 && $nilai < 80):
+                $kategori = "C";
+                break;
+            default:
+                $kategori = "D";
+        }
+
+        // tampilkan hasil
+        echo "Nilai Anda adalah " . $nilai . ", kategori nilai Anda adalah " . $kategori;
+    }
+    ?>
+</body>
+</html>
