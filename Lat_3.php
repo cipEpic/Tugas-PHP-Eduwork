8a08cd571fb872c711fa95b42cf3b4b5e669e100
Evi Dianasari <evidnsr@gmail.com>
2023-04-25 23:20:19 +0800

    Update Lat_3.php

--- Lat_3.php
+++ Lat_3.php
@@ -1,7 +1,10 @@
 <?php
-
-
-
-
-
+for($i=0;$i<=9;$i++){
+    for($j=0;$j<$i;$j++){
+        echo "$j";
+    }
+    echo "<br/>";
+}
 ?>
+
+<!-- Beserta buatlah komentar tiap line dari perintah yang ada, jelaskan maksud dari setiap baris scriptnya. -->
