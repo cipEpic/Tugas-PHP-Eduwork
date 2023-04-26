c94cd70e91dafdbc3ff493c3b01ec3885d3d42bd
Evi Dianasari <evidnsr@gmail.com>
2023-04-26 22:33:34 +0800

    Update bangun_datar.php

--- tugas_operator/bangun_datar.php
+++ tugas_operator/bangun_datar.php
@@ -1,33 +1,50 @@
+<!-- Istilah-istilah baru
+pow($r, 2) artinya r pangkat 2
+pi() = 3.14
+. digunakan untuk menyambungkan string 
+-->
+
 <?php
 // Luas persegi
-function area_square($side) {
-    return $side * $side;
+function luas_square($sisi) {
+    return pow($sisi, 2);
 }
 
 // Luas persegi panjang
-function area_rectangle($length, $width) {
-    return $length * $width;
+function luas_rectangle($panjang, $lebar) {
+    return $panjang * $lebar;
 }
 
-// Luas lingkaran
-function area_circle($radius) {
-    return pi() * pow($radius, 2);
+// Luas lingkaran dengan phi
+function luas_circle1($r) {
+    return pi() * pow($r, 2);
+}
+
+// Luas lingkaran 22/7
+function luas_circle2($r) {
+    return (22/7) * pow($r, 2);
 }
 
 // Luas segitiga
-function area_triangle($base, $height) {
-    return 0.5 * $base * $height;
+function luas_triangle($alas, $tinggi) {
+    return 0.5 * $alas * $tinggi;
 }
 
 // Luas Trapesium
-function area_trapesium($base1, $base2, $height) {
-    return 0.5 * ($base1 + $base2) * $height;
+function luas_trapesium($a, $b, $tinggi) {
+    return 0.5 * ($a + $b) * $tinggi;
 }
 
-// Sample usage
-echo "Luas persegi dengan panjang sisi 5 adalah: " . area_square(5) . "<br>";
-echo "Luas persegi panjang dengan panjang 8 dan lebar 4 adalah: " . area_rectangle(8, 4) . "<br>";
-echo "Luas lingkaran dengan jari-jari 3 adalah : " . area_circle(3) . "<br>";
-echo "Luas segitiga dengan alas 6 dan tinggi 2 adalah : " . area_triangle(6, 2) . "<br>";
-echo "Luas trapesium dengan alas 3, alas 2 5, dan tinggi 4 adalah: " . area_trapesium(3, 5, 4) . "<br>";
+// Input nilai
+echo "1. Luas persegi dengan panjang sisi 3 adalah: ";
+echo luas_square(3);
+echo "<br>";
+
+echo "2. Luas persegi panjang dengan panjang 6 dan lebar 4 adalah: " . luas_rectangle(6, 4) . "<br>";
+echo "3. Luas lingkaran dengan jari-jari 7 dan phi 3,14 adalah : " . luas_circle1(7) . "<br>";
+echo "3. Luas lingkaran dengan jari-jari 7 dan phi 22/7 adalah : " . luas_circle2(7) . "<br>";
+echo "4. Luas segitiga dengan alas 3 dan tinggi 4 adalah : " . luas_triangle(3, 4) . "<br>";
+echo "5. Luas trapesium dengan alas(a) 3, alas(b) 4, dan tinggi 6 adalah: " . luas_trapesium(3, 4, 6) . "<br>";
 ?>
+
+
