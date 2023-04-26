444c2aaed9ec59ce30d7dfca2edb2ee79bfcef50
Evi Dianasari <evidnsr@gmail.com>
2023-04-26 23:24:32 +0800

    Update bangun_ruang.php

--- tugas_operator/bangun_ruang.php
+++ tugas_operator/bangun_ruang.php
@@ -1,21 +1,21 @@
 <?php
 // Volume kubus
-function volume_cube($side) {
-    return pow($side, 3);
+function volume_kubus($s) {
+    return pow($s, 3);
 }
 
 // Volume balok
-function volume_beam($width, $height, $length) {
-    return $width * $height * $length;
+function volume_balok($lebar, $tinggi, $panjang) {
+    return $lebar * $tinggi * $panjang;
 }
 
 // Volume tabung
-function volume_cylinder($radius, $height) {
-    return pi() * pow($radius, 2) * $height;
+function volume_tabung($r, $t) {
+    return (22/7) * pow($r, 2) * $t;
 }
 
 // Sample usage
-echo "Volume kubus dengan panjang sisi 5 adalah: " . volume_cube(5) . "<br>";
-echo "Volume balok dengan lebar 5, tinggi 3, dan panjang 10 adalah:" . volume_beam(5, 3, 10) . "<br>";
-echo "Volume tabung dengan jari-jari 2 dan tinggi 4 adalah: " . volume_cylinder(2, 4) . "<br>";
+echo "1. Volume kubus dengan panjang sisi 5 adalah: " . volume_kubus(5) . "<br>";
+echo "2. Volume balok dengan lebar 6, tinggi 4, dan panjang 10 adalah: " . volume_balok(6, 4, 10) . "<br>";
+echo "3. Volume tabung dengan jari-jari 7 dan tinggi 4 adalah: " . volume_tabung(7, 4) . "<br>";
 ?>
